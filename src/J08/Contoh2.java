45f8ed1d19723e5f137336a90760de18f91b5d09
WINDOWS 10 <WINDOWS 10@DESKTOP-BPSCJHN>
2018-08-02 08:17:39 +0700

    For While Do-While

---
+++ src/J08/Contoh2.java
@@ -0,0 +1,34 @@
+package J08;
+
+public class Contoh2 {
+     public static void main(String[] args)
+    {
+        //PERULANGAN FOR
+        int x;
+        for(x = 1; x <= 10; x++)
+        {
+            System.out.print(x + " ");
+        }
+        
+         // PERULANGAN WHILE
+       
+        while(x <= 10)
+        {
+            System.out.println(x + " ");
+            x++;
+        }
+        
+        //PERULANGAN DO WHILE
+        do
+        {
+            System.out.println(x + " ");
+            x++;
+        }
+        while(x <= 10);
+        
+        }
+    }
+
+        
+        
+
